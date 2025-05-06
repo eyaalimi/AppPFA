f0f5f7565ae9745bf36fc99ca8f73e412071c3a8
eya <alimieyaa@gmail.com>
2025-05-06 21:03:59 +0100

    eyafinal t

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,6 @@
 # output "private_key_path" {
 #   value = var.private_key_path
 # }
-output "kubernetes_ip" {
-  value = aws_instance.kubernetes.public_ip
-}
-
+# output "instance_public_ip" {
+#   value = module.ec2_kubernetes[0].instance_public_ip
+# }
